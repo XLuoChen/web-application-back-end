76a6be23c5b7875e7675091c54bd2fdbc43de39e
Chengxiu Luo <cxluo@thoughtworks.com>
2017-09-25 11:05:01 +0800

    Modified deploy file

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,15 +4,15 @@ if [ ! -d /var/www/html ]; then
     sudo mkdir -p /var/www/html
 fi
 
-rm -rf /var/www/html/leafer
+sudo rm -rf /var/www/html/leafer
 sudo cp -r /var/lib/jenkins/workspace/leafer /var/www/html
 
 cd /var/www/html/leafer
 
-mysql < ./db_config.sql
-mysql -u root -p root < ./leafer.sql
+mysql < /var/lib/jenkins/workspace/leafer/scripts/db_config.sql
+mysql -u root -p root < /var/lib/jenkins/workspace/leafer/scripts/leafer.sql
 
-sudo apt install maven
+sudo apt install -y maven
 
 mvn clean install
 mvn spring-boot:run
