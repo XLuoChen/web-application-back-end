8789a28a6660a34bb913ba326c3b83f8086f9755
Chengxiu Luo <cxluo@thoughtworks.com>
2017-09-27 21:49:42 +0800

    Use shell script to migrate db

---
+++ scripts/db_config.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+mysql -u root -proot < /var/lib/jenkins/workspace/leafer/scripts/leafer.sql
--- scripts/db_config.sql
+++
@@ -1,7 +0,0 @@
--- USE mysql;
---
-DROP USER 'root'@'localhost';
-CREATE USER 'root'@'localhost' IDENTIFIED BY 'root';
-GRANT ALL PRIVILEGES ON *.* TO 'root'@'localhost';
-
-FLUSH PRIVILEGES;
