a66928bb754cb50e4c2691a91f9d17447a3d10b3
Chengxiu Luo <cxluo@thoughtworks.com>
2017-09-28 16:06:53 +0800

    Use ansible to deploy project

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
+ssh remote@192.168.30.12
+
 if [ ! -d /tmp/target ]; then
     sudo mkdir -p /tmp/target
 fi
 
 sudo rm -rf /target
-sudo cp -r /var/lib/jenkins/workspace/leafer/target/leafer-0.1.0.jar /tmp/target
+scp -r /var/lib/jenkins/workspace/leafer/target/leafer-0.1.0.jar 192.168.30.12:/tmp/target
 
-cd /tmp/target
+cd 192.168.30.12:/tmp/target
 
 java -jar leafer-0.1.0.jar &
 
